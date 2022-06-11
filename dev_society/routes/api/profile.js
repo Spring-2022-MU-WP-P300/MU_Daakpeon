0bb326056ab339988cc6f8834eb004fcc875bbd9
Iffat Ahmed Chowdhury Nahid <nahidiac@gmail.com>
2022-06-11 13:07:54 +0600

    created Profile model

--- dev_society/routes/api/profile.js
+++ dev_society/routes/api/profile.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 const router = express.Router();
 
-router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
+// Load Profile Model
+const Profie = require('../../models/Profile');
 
+// Load User Profile
+const User = require('../../models/User');
+
+router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
 module.exports = router;
