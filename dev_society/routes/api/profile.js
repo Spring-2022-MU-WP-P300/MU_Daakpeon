64f3798092694eebcdb5a87d4382d0abd1aa5d8f
Iffat Ahmed Chowdhury Nahid <64349393+nahidiac@users.noreply.github.com>
2022-06-11 14:18:04 +0600

    Tanvir starting work testing

--- dev_society/routes/api/profile.js
+++ dev_society/routes/api/profile.js
@@ -5,10 +5,25 @@ const passport = require('passport');
 const router = express.Router();
 
 // Load Profile Model
-const Profie = require('../../models/Profile');
+const Profile = require('../../models/Profile');
 
 // Load User Profile
 const User = require('../../models/User');
 
 router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
+
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+        .then((profile) => {
+            if (!profile) {
+                errors.noprofile = 'Profile does not exist';
+                return res.status(404).json(errors);
+            }
+            return res.status(200).json(profile);
+        })
+        .catch((err) => res.status(404).json(err));
+});
+
 module.exports = router;
