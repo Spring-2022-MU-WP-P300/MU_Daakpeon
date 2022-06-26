bfa08775b78c19ee1f2c53939ba8136c3c572c5a
7anvir <greencrystaltanvir@gmail.com>
2022-06-26 11:43:05 +0600

    login, signup page done, redux devtools setup done

--- dev_society/frontend/src/App.js
+++ dev_society/frontend/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import store from './store';
+
 
 
 class App extends Component {
   render() {
     return (
-      <>
-      <Navbar/>
-      <Landing/>
-      <Footer/>
-      </>
+      <Provider store={store}>
+      <Router>
+        <div className='App'>
+          <Navbar/>
+          <Routes>
+            <Route exact path="/" element={<Landing/>} />
+              <Route exact path="/register" element={<Register/>} />
+              <Route exact path="/login" element={<Login/>} />
+          </Routes>
+          <Footer/>
+        </div>
+      </Router>
+      </Provider>
     )
   }
 }
